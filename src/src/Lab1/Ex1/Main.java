3bc3702794e8735d961249b59e5faf4c038d7df3
andybykov <portperm@yandex.ru>
2026-01-19 18:25:19 +0300

    Add exercise 1

---
+++ src/src/Lab1/Ex1/Main.java
@@ -0,0 +1,10 @@
+package Lab1.Ex1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Я");
+        System.out.println("хорошо");
+        System.out.println("знаю");
+        System.out.println("Java.");
+    }
+}
