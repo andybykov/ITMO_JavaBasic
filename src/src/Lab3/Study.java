0b6439a7f222481e34b05a9cff15fd0cbd3c14e9
andybykov <portperm@yandex.ru>
2026-01-23 20:41:51 +0300

    Lab3: before chek

---
+++ src/src/Lab3/Study.java
@@ -0,0 +1,15 @@
+package Lab3;
+
+// Задание 1
+public class Study {
+
+    private String course;
+
+    Study(String course){
+        this.course = course;
+    }
+
+    public String printCourse() {
+        return this.course;
+    }
+}
