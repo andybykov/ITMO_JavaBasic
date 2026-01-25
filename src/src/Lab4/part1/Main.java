ffaf91a544a1ea7bb9b587b11f910e27171bbb33
andybykov <portperm@yandex.ru>
2026-01-25 11:26:56 +0300

    Lab 4: complete part 1

---
+++ src/src/Lab4/part1/Main.java
@@ -0,0 +1,12 @@
+package Lab4.part1;
+
+public class Main{
+    public static void main (String[] args){
+        PrintNumbers.exercise1();
+        PrintNumbers.exercise4();
+        PrintNumbers.exercise3();
+        PrintNumbers.exercise5();
+        PrintNumbers.exercise6();
+        PrintNumbers.exercise2();
+    }
+}
