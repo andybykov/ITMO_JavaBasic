9d876d32c1610810685d0bdfee647bf3ee81b87f
andybykov <portperm@yandex.ru>
2026-02-04 23:51:25 +0300

    Lab 7: complete

--- src/src/Lab7/Main.java
+++ src/src/Lab7/Main.java
@@ -1,4 +1,11 @@
 package Lab7;
 
 public class Main {
+    public static void main(String[] args) {
+        Airplane airplane1 = new Airplane("Boenig 737-800", 48000);
+        Airplane airplane2 = new Airplane("Airbus 320", 50000);
+
+        airplane1.showWeight();
+        airplane2.showWeight();
+    }
 }
